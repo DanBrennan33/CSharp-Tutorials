d62f497b1da5f579ec427d0b6fd399236d3deaa9
DanBrennan33 <DanBrennan33@hotmail.com>
2018-01-22 21:49:59 -0500

    LINQ tutorial completed for Faro Shuffle and logging each query.

---
+++ LINQ/PlayingCard.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LINQ
+{
+    public class PlayingCard
+    {
+        public Suit CardSuit { get; set; }
+        public Rank CardRank { get; set; }
+
+        public PlayingCard(Suit s, Rank r)
+        {
+            CardSuit = s;
+            CardRank = r;
+        }
+
+        public override string ToString() => $"{ CardRank } of { CardSuit }";
+    }
+}
---
+++ LINQ/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ
+{
+    public enum Suit
+    {
+        Clubs, 
+        Diamonds,
+        Hearts,
+        Spades
+    }
+
+    public enum Rank
+    {
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King,
+        Ace
+    }
+
+    public class Program
+    {
+        static IEnumerable<Suit> Suits() => Enum.GetValues(typeof(Suit)) as IEnumerable<Suit>;
+
+        static IEnumerable<Rank> Ranks() => Enum.GetValues(typeof(Rank)) as IEnumerable<Rank>;
+        
+        static void Main(string[] args)
+        {
+            var startingDeck = (from s in Suits().LogQuery("Suit Generation")
+                                from r in Ranks().LogQuery("Rank Generation")
+                                select new PlayingCard(s, r))
+                                .LogQuery("Starting Deck")
+                                .ToArray();
+                                
+            // foreach (var c in startingDeck) 
+            // {
+            //     Console.WriteLine(c);
+            // }
+            
+            var shuffle = startingDeck;
+            var times = 0;
+
+            do {
+                shuffle = shuffle.Take(26)
+                    .LogQuery("Top Half")
+                    .InterleaveSequenceWith(shuffle.Skip(26).LogQuery("Bottom Half"))
+                    .LogQuery("shuffle")
+                    .ToArray();
+
+                // foreach (var c in shuffle)
+                // {
+                //    Console.WriteLine(c);
+                // }
+
+                times++;
+            } while (!startingDeck.SequenceEquals(shuffle));
+
+            Console.WriteLine(times);
+        }
+    }
+}
